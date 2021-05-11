dee66e37035cd1a206c1d2a0edc9d23edc52c857
Vitor dos Santos Barcelos <progvitor@gmail.com>
2021-05-11 15:29:40 -0300

    Praticising

--- Learning_and_praticising_/Decorators/Decorators.py
+++ Learning_and_praticising_/Decorators/Decorators.py
@@ -4,10 +4,17 @@ import datetime
 def log(func):
     def wrapper():
         with open("./log.txt", 'a') as log:
-            log.writeline(
+            if 'class' in str(type(func)):
+                log.writelines(
+                    f'Class {func.__name__} initialized at '
+                    f'{datetime.datetime.now()} \n'
+                )
+
+            log.writelines(
                 f'Function {func.__name__} was called at '
-                f'{datetime.datetime.now()}'
+                f'{datetime.datetime.now()} \n'
             )
+
         return func
 
     return wrapper()
@@ -18,4 +25,17 @@ def random_func(value_1, value_2):
     print(value_2 + value_1)
 
 
+@log
+class SomeClass:
+    def __init__(self, id):
+        self.id = id
+
+    def show_id(self):
+        print(self.id)
+
+
 random_func(2, 1)
+
+class_ = SomeClass(1101)
+
+class_.show_id()
--- teste.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-output = subprocess.run(
-    ['netsh', 'wlan', 'show', 'profile', 'Fibernet', 'key=clear'],
-    capture_output=True).stdout.decode('ISO-8859-1')
-print(output)
